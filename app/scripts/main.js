a49e1491e403015cc55cf9d828ea7e6808185168
Thomas Lebeau <lebeau.thomas@gmail.com>
2013-05-02 10:27:01 +0300

    initial commit

---
+++ app/scripts/main.js
@@ -0,0 +1,20 @@
+'use strict';
+
+(function($){
+
+  /**
+   * Small or Big Hack!
+   */
+  // $('#small').click(function(e){
+  //   e.preventDefault();
+  //   $('header').addClass('small');
+  //   $('#cta').attr('src', 'images/header/badge-chrome.png');
+  // });
+
+  // $('#big').click(function(e){
+  //   e.preventDefault();
+  //   $('header').removeClass('small');
+  //   $('#cta').attr('src', 'images/header/badge-chrome@2x.png');
+  // });
+
+})(jQuery);
