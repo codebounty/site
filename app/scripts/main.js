89bb51094a695ed8dd6bf17a901a2c1a4534de77
Thomas Lebeau <lebeau.thomas@gmail.com>
2013-05-06 21:05:12 +0300

    version 0.3

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -16,5 +16,31 @@
   //   $('header').removeClass('small');
   //   $('#cta').attr('src', 'images/header/badge-chrome@2x.png');
   // });
+  //
+
+  /**
+   * Browserwrapper
+   */
+  $('#browser-github').browserWrapper({
+    browserTitle:     'Windows Support · Issue #216 · yeoman/yeoman',
+    browserURL:       'https://github.com/yeoman/yeoman/issues/216',
+    filePath:         'components/browserWrapper-lite/src/',
+    shadow:           '0',
+  });
+
+  $('#browser-capture').browserWrapper({
+    browserTitle:     'Windows Support · Issue #216 · yeoman/yeoman',
+    browserURL:       'https://github.com/yeoman/yeoman/issues/216',
+    filePath:         'components/browserWrapper-lite/src/',
+    shadow:           '0',
+  });
+
+  /**
+   * Scroll to detail section
+   */
+  $('.howto').click(function(e) {
+    e.preventDefault();
+    $('html,body').stop().animate({scrollTop: $('#howto').offset().top}, 500);
+  });
 
 })(jQuery);
